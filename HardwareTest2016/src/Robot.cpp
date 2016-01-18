341746f32324afc41fb013af54ac71996a0d8d38
JKSalmon <pike@mtco.com>
2016-01-18 15:08:56 -0600

    Added support for PCM motors 1 through 9.

--- HardwareTest2016/src/Robot.cpp
+++ HardwareTest2016/src/Robot.cpp
@@ -5,9 +5,18 @@ const int CANID_DRIVE_TRAIN_FRONT_RIGHT = 2;
 const int CANID_DRIVE_TRAIN_REAR_LEFT   = 3;
 const int CANID_DRIVE_TRAIN_REAR_RIGHT  = 4;
 
-const int PCMCHANNEL_SOME_MOTOR = 0;
+const int PCMCHANNEL_SOME_MOTOR_0 = 0;
+const int PCMCHANNEL_SOME_MOTOR_1 = 1;
+const int PCMCHANNEL_SOME_MOTOR_2 = 2;
+const int PCMCHANNEL_SOME_MOTOR_3 = 3;
+const int PCMCHANNEL_SOME_MOTOR_4 = 4;
+const int PCMCHANNEL_SOME_MOTOR_5 = 5;
+const int PCMCHANNEL_SOME_MOTOR_6 = 6;
+const int PCMCHANNEL_SOME_MOTOR_7 = 7;
+const int PCMCHANNEL_SOME_MOTOR_8 = 8;
+const int PCMCHANNEL_SOME_MOTOR_9 = 9;
 
-const int PORTID_JOYSTICK_1 = 5;
+const int PORTID_JOYSTICK_1 = 0;
 
 class Robot: public IterativeRobot
 {
@@ -15,7 +24,16 @@ class Robot: public IterativeRobot
 	SpeedController* driveTrainFrontRight;
 	SpeedController* driveTrainRearLeft;
 	SpeedController* driveTrainRearRight;
-	SpeedController* someMotor;
+	SpeedController* someMotor0;
+	SpeedController* someMotor1;
+	SpeedController* someMotor2;
+	SpeedController* someMotor3;
+	SpeedController* someMotor4;
+	SpeedController* someMotor5;
+	SpeedController* someMotor6;
+	SpeedController* someMotor7;
+	SpeedController* someMotor8;
+	SpeedController* someMotor9;
 	RobotDrive myRobot; // robot drive system
 	Joystick stick; // only joystick
 	LiveWindow *lw;
@@ -28,7 +46,16 @@ public:
 		driveTrainFrontRight(new CANTalon(CANID_DRIVE_TRAIN_FRONT_RIGHT)),
 		driveTrainRearLeft(new CANTalon(CANID_DRIVE_TRAIN_REAR_LEFT)),
 		driveTrainRearRight(new CANTalon(CANID_DRIVE_TRAIN_REAR_RIGHT)),
-		someMotor(new Victor(PCMCHANNEL_SOME_MOTOR)),
+		someMotor0(new Victor(PCMCHANNEL_SOME_MOTOR_0)),
+		someMotor1(new Victor(PCMCHANNEL_SOME_MOTOR_1)),
+		someMotor2(new Victor(PCMCHANNEL_SOME_MOTOR_2)),
+		someMotor3(new Victor(PCMCHANNEL_SOME_MOTOR_3)),
+		someMotor4(new Victor(PCMCHANNEL_SOME_MOTOR_4)),
+		someMotor5(new Victor(PCMCHANNEL_SOME_MOTOR_5)),
+		someMotor6(new Victor(PCMCHANNEL_SOME_MOTOR_6)),
+		someMotor7(new Victor(PCMCHANNEL_SOME_MOTOR_7)),
+		someMotor8(new Victor(PCMCHANNEL_SOME_MOTOR_8)),
+		someMotor9(new Victor(PCMCHANNEL_SOME_MOTOR_9)),
 		myRobot(driveTrainFrontLeft, driveTrainFrontRight),
 		stick(PORTID_JOYSTICK_1),
 		lw(LiveWindow::GetInstance()),
