12bac9ac9e2ad36ccb6b64b49b32a7aee79d05ca
JKSalmon <pike@mtco.com>
2016-01-18 14:19:24 -0600

    Added support for 4 CANs, 1 PCM.

--- HardwareTest2016/src/Robot.cpp
+++ HardwareTest2016/src/Robot.cpp
@@ -1,8 +1,21 @@
 #include "WPILib.h"
 
+const int CANID_DRIVE_TRAIN_FRONT_LEFT  = 1;
+const int CANID_DRIVE_TRAIN_FRONT_RIGHT = 2;
+const int CANID_DRIVE_TRAIN_REAR_LEFT   = 3;
+const int CANID_DRIVE_TRAIN_REAR_RIGHT  = 4;
+
+const int PCMCHANNEL_SOME_MOTOR = 0;
+
+const int PORTID_JOYSTICK_1 = 5;
+
 class Robot: public IterativeRobot
 {
-
+	SpeedController* driveTrainFrontLeft;
+	SpeedController* driveTrainFrontRight;
+	SpeedController* driveTrainRearLeft;
+	SpeedController* driveTrainRearRight;
+	SpeedController* someMotor;
 	RobotDrive myRobot; // robot drive system
 	Joystick stick; // only joystick
 	LiveWindow *lw;
@@ -10,17 +23,26 @@ class Robot: public IterativeRobot
 
 public:
 	Robot() :
-		myRobot(0, 1),	// these must be initialized in the same order
-		stick(0),		// as they are declared above.
+		// These must be initialized in the same order as they are declared above.
+		driveTrainFrontLeft(new CANTalon(CANID_DRIVE_TRAIN_FRONT_LEFT)),
+		driveTrainFrontRight(new CANTalon(CANID_DRIVE_TRAIN_FRONT_RIGHT)),
+		driveTrainRearLeft(new CANTalon(CANID_DRIVE_TRAIN_REAR_LEFT)),
+		driveTrainRearRight(new CANTalon(CANID_DRIVE_TRAIN_REAR_RIGHT)),
+		someMotor(new Victor(PCMCHANNEL_SOME_MOTOR)),
+		myRobot(driveTrainFrontLeft, driveTrainFrontRight),
+		stick(PORTID_JOYSTICK_1),
 		lw(LiveWindow::GetInstance()),
 		autoLoopCounter(0)
 	{
+		printf("Robot ctor ENTER\n");
 		myRobot.SetExpiration(0.1);
+		printf("Robot ctor EXIT\n");
 	}
 
 private:
 	void AutonomousInit()
 	{
+		printf("Robot::AutonomousInit\n");
 		autoLoopCounter = 0;
 	}
 
@@ -35,14 +57,36 @@ private:
 		}
 	}
 
+	void DisabledInit()
+	{
+		printf("Robot::DisabledInit\n");
+	}
+
+	void DisabledPeriodic()
+	{
+		//printf("Robot::DisabledPeriodic\n");
+	}
+
+	void RobotInit()
+	{
+		printf("Robot::RobotInit ENTER\n");
+		lw = LiveWindow::GetInstance();
+		printf("Robot::RobotInit EXIT\n");
+	}
+
 	void TeleopInit()
 	{
-
+		printf("Robot::TeleopInit\n");
 	}
 
 	void TeleopPeriodic()
 	{
-		myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
+		myRobot.ArcadeDrive(stick); // Arcade-style driving
+	}
+
+	void TestInit()
+	{
+		printf("Robot::TestInit\n");
 	}
 
 	void TestPeriodic()
